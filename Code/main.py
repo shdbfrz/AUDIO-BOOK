fa9edfc3a64d8cfa253a02b7fa01858bd9db0ee5
Shadab Firoz <148373760+shdbfrz@users.noreply.github.com>
2025-02-03 02:06:39 +0530

    Add files via upload

---
+++ Code/main.py
@@ -0,0 +1,17 @@
+import pyttsx3 
+import PyPDF2
+from tkinter.filedialog import askopenfilename
+
+book = askopenfilename()
+pdfreader = PyPDF2.PdfReader(book)
+pages = len(pdfreader.pages)  
+player = pyttsx3.init()
+
+for num in range(pages):
+    page = pdfreader.pages[num]  
+    text = page.extract_text() 
+    player.say(text)
+    
+player.runAndWait()
+
+
